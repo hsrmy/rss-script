c1b18279c4aeedbfade2a84d8bacfb8c408af0c1
Hirotaka Takase <e165755@ie.u-ryukyu.ac.jp>
2016-12-03 23:25:51 +0900

    ameblo取得時に異常あり

--- test.sh
+++ test.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 pre=$(mktemp) temp=$(mktemp)
 webhook="https://hooks.slack.com/services/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"
-for site in nana sphere trysail planet circus; do
-  file="/Users/hsrmy/rss/${site}.xml"
+
+favicon(){
+  case $1 in
+    nana )
+      favicon="http://mizukinana.jp/favicon.ico"
+      channel="#mizukinana";;
+    sphere )
+      favicon="https://sphere.m-rayn.jp/assets/sphere/favicon-4493e5b29f8b41089dbbba304f78a344.png"
+      channel="#sphere";;
+    planet )
+      favicon="http://lantis-net.com/favicon.ico"
+      channel="#sphere";;
+    trysail )
+      favicon="https://trysail.jp/assets/trysail/favicon-8878bcce2ddb484e9eb3ec1b328066cc.png"
+      channel="#trysail";;
+    circus )
+      favicon="https://emradc.wjg.jp/circus-favicon.ico"
+      channel="#general";;
+    minako|ayahi|haruka|aki )
+      favicon="http://ameblo.jp/favicon.ico"
+      channel="#sphere";;
+    mocho|sora|nansu )
+      favicon="http://ameblo.jp/favicon.ico"
+      channel="#trysail";;
+  esac
+}
+
+list=("nana" "sphere" "trysail" "planet" "circus") #公式サイト
+list+=("minako" "ayahi" "haruka" "aki") #スフィア
+list+=("mocho" "sora" "nansu") #TrySail
+for site in ${list[@]} ; do
+  echo $site
+  file=$(dirname $0)/rss/${site}.xml
   if [ ! -e $file ]; then
+    if [ ! -d $dir ]; then
+      mkdir $dir
+    fi
     touch $file
+    curl -Ss -X GET "https://emradc.wjg.jp/dhcp/rss.xml?site=${site}" > $file
+    text="${site}.xmlが見つからないため、作成しました。"
+    data="payload={\"username\": \"RSS Notification\",\"text\": \"$text\"}"
+    curl -Ss -X POST --data-urlencode "$data" $webhook > /dev/null
   fi
   curl -Ss -X GET "https://emradc.wjg.jp/var/rss.xml?site=${site}" > $pre
   title=$(echo "cat /rss/channel/title"|xmllint --shell $pre|grep '<title>'|sed -e "s/<title>\(.*\)<\/title>/\1/")
@@ -28,10 +66,11 @@ for site in nana sphere trysail planet circus; do
         text+=("$text_pre\n")
       fi
     done
-    data="payload={\"channel\": \"#random\",\"username\": \"$title\",\"icon_url\": \"$favicon\",\"text\": \"${text[@]}\"}"
+    favicon ${site}
+    data="payload={\"channel\": \"$channel\",\"username\": \"$title\",\"icon_url\": \"$favicon\",\"text\": \"${text[@]}\"}"
     curl -Ss -X POST --data-urlencode "$data" $webhook > /dev/null
     IFS=$IFS_ORG
-    cp $temp $file
+    cp $pre $file
   fi
 done
 rm $temp $pre
